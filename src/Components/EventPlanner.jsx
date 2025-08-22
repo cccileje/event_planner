353b3279e39d9f019d0b548d1d4e522d5e450cfe
cccileje <cccileje@gmail.com>
2025-08-22 19:41:22 -0400

    add contact form

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -73,7 +73,13 @@ const EventPlanner = () => {
             </section>
             {/* Section to provide contact info/form */}
             <section className='contact'>
-                {/* Contact content here */}
+                <h2>Contact Us</h2>
+                <form>
+                    <input type="text" placeholder='Name' />
+                    <input type="email" placeholder="Email" />
+                    <textarea placeholder='Message'></textarea>
+                    <button className='submit-button'>Send</button>
+                </form>
             </section>
         </div>
     );
